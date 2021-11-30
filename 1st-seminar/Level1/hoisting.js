396ee7c467b20c22b93fe4e1f65bbc10e7cdbe36
kanghanhee <kanghanhee59@gmail.com>
2021-11-30 15:37:26 +0900

    .

--- 1st-seminar/Level1/hoisting.js
+++
@@ -1,9 +0,0 @@
-// same code interpreted by the JS engine
-function hoistFunction() {
-    var x;
-    console.log(x);
-    x = "var";
-    console.log(x);
-}
-
-hoistFunction();
--- 2nd-seminar/Level1/requireModule.js
+++
@@ -1,1 +0,0 @@
-const moduleName = require("./module");
