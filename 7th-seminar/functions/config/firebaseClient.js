68d96a37fe1c0c18200b9e060817a8b2bdbf3bea
kanghanhee <kanghanhee59@gmail.com>
2021-12-24 20:59:55 +0900

    7차 세미나 과제

---
+++ 7th-seminar/functions/config/firebaseClient.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDi5-BRtXgUdxSq-hmkccqnW_ZzcXKvrE4",
+  authDomain: "wesopt29-899e9.firebaseapp.com",
+  projectId: "wesopt29-899e9",
+  storageBucket: "wesopt29-899e9.appspot.com",
+  messagingSenderId: "719832770928",
+  appId: "1:719832770928:web:ecd0859e90c386bfd687b5",
+  measurementId: "G-6QB7N0TK17",
+};
